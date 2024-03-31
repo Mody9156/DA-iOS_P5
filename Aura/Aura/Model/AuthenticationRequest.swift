178d3605d28c6edafb6ef9e352c4243db276ef90
Modibo KEITA <modykeita2@gmail.com>
2024-03-31 13:13:34 +0200

    update

---
+++ Aura/Aura/Model/AuthenticationRequest.swift
@@ -0,0 +1,14 @@
+//
+//  AuthenticationRequest.swift
+//  Aura
+//
+//  Created by KEITA on 28/03/2024.
+//
+
+import Foundation
+
+struct AuthenticationRequest {
+    let username: String
+    let password: String
+}
+
--- Aura/Aura/ViewModels/AuthenticationViewModel.swift
+++ Aura/Aura/ViewModels/AuthenticationViewModel.swift
@@ -10,20 +10,47 @@ import Foundation
 class AuthenticationViewModel: ObservableObject {
     @Published var username: String = ""
     @Published var password: String = ""
-     var urlModel =  UrlModel
+    var AutentificationUrl = URL(string: "http://127.0.0.1:8080/auth")!
+    
+    private let session : URLSession
+    
     
     var onLoginSucceed: (() -> ())
     
-    init(_ callback: @escaping () -> ()) {
+    init(_ callback: @escaping () -> (),session:URLSession = URLSession(configuration: .ephemeral)){
+        self.session = session
         self.onLoginSucceed = callback
     }
+    
+    private func Autentification() async throws -> AuthenticationRequest {
+        let (data,_) = try await session.data(for: geturl())
+        
+        guard let Jsonresponse = try? JSONDecoder().decode([String:String].self, from: data),
+           let username = Jsonresponse["est@aura.app"],
+           let password = Jsonresponse["test123"] else {
+            return  AuthenticationRequest(username: username, password: password)
+        }
+        
+        return AuthenticationRequest(username: username, password: password)
+    }
+    
+    
+    
+    private func geturl() -> URLRequest{
+        var request = URLRequest(url: AutentificationUrl)
+        request.httpMethod = "POST"
+        
+        let parameter = "username=test@aura.app&password=test123"
+        request.httpBody = parameter.data(using: .utf8)
+        return request
+    }
+    
+    
 
-    
-    
     func login() {
         print("login with \(username) and \(password)")
         onLoginSucceed()
+      
         
-       
     }
 }
