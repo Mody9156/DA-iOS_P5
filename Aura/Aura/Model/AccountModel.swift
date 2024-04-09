46e7f2bfd700af62c7cd5f7da50ffce894bf0842
Modibo KEITA <modykeita2@gmail.com>
2024-04-09 10:40:36 +0200

    update

--- Aura/Aura/Model/AccountModel.swift
+++ Aura/Aura/Model/AccountModel.swift
@@ -4,24 +4,41 @@
 //
 //  Created by KEITA on 03/04/2024.
 //
-
 import Foundation
 
-final class accountModel {
+final class AccountModel {
     let url = URL(string: "http://127.0.0.1:8080/account")!
-    let session : URLSession
+    let session: URLSession
+    let authenticationRequest: AuthenticationRequest
     
-    init(session: URLSession) {
+    init(session: URLSession = URLSession(configuration: .ephemeral), authenticationRequest: AuthenticationRequest) {
         self.session = session
+        self.authenticationRequest = authenticationRequest
     }
     
-    func getRequest() -> URLRequest{
-        let request = URLRequest(url: url)
+    enum Failure: Error {
+        case tokenInvalid
+    }
+    
+    func fetchAccountDetails(username: String, password: String) async throws -> (value: String, label: String) {
+        let token = try await authenticationRequest.getToken(username: username, password: password)
+        let (data, _) = try await session.data(for: getRequest(token: token))
+
+        guard let json = try? JSONDecoder().decode([String: String].self, from: data),
+              let value = json["value"] ,
+              let label = json["label"] else {
+            throw Failure.tokenInvalid
+        }
+
+       
+        return (value,label)
+       
+    }
+    
+    private func getRequest(token: String) -> URLRequest {
+        var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        
+        request.setValue(token, forHTTPHeaderField: "token")//headers
         return request
     }
-    
 }
-
-// terminer l'appel reseau à l'aide de postname et reorganiser le code de manère structuré
